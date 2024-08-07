20cee9c309535caaa7d0602e5f302e36b756b03f
B Mack <lumeo@me.com>
2024-08-07 12:41:03 -0400

    add tableId and stripeKey

--- apps/next/app/products/page.tsx
+++ apps/next/app/products/page.tsx
@@ -1,11 +1,33 @@
 'use client'
 
 import { ProductsScreen } from 'app/features/products/screen'
+import { useEffect, useState } from 'react'
+import { Spinner } from 'tamagui'
 
 export default function Products() {
+  const [id, setId] = useState('')
+  const [key, setKey] = useState('')
+  // Get the pricing table id and test key from ENV
+  useEffect(() => {
+    if (process.env) {
+      setId(process.env.NEXT_PUBLIC_STRIPE_PRICING_TABLE_ID as string)
+      setKey(process.env.NEXT_PUBLIC_STRIPE_TEST_KEY as string)
+    }
+  }, [])
+
+  console.log({ id, key})
+
   return (
     <>
-      <ProductsScreen />
+      {id && key ? (
+        <ProductsScreen
+          tableId={id}
+          stripeKey={key}
+        />
+      ) : (
+        <Spinner />
+      )}
+
     </>
   )
 }
--- packages/app/features/products/screen.tsx
+++ packages/app/features/products/screen.tsx
@@ -6,13 +6,20 @@ declare global {
   }
 }
 
-export function ProductsScreen() {
+export function ProductsScreen({
+  tableId, stripeKey
+}: {
+  tableId: string;
+  stripeKey: string;
+}) {
+
   return (
     <>
       <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
-      <stripe-pricing-table pricing-table-id="prctbl_1PiNWkCgzhWoiiPejP5lpFeB"
-        publishable-key="pk_live_51NtBZnCgzhWoiiPeaHa1ZbXPMwWHpyACXsY8gCikKOmcCjnGI83SZfYnDUso4d9ow4tNSQtwTk9eJyFyAt3Jqo2R00SyuFSPYG">
-      </stripe-pricing-table>
+      <stripe-pricing-table
+        pricing-table-id={tableId}
+        publishable-key={stripeKey}
+      />
     </>
   )
 }
